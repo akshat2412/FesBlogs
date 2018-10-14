966267705ed7e0f8141f991ac5ea17705d58e525
Akshat Gupta <akshat.gupta.241294@gmail.com>
2018-10-14 10:14:44 +0530

    added article-list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { ArticleListComponent } from './article-list/article-list.component';
+import { ArticleListItemComponent } from './article-list-item/article-list-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ArticleListComponent,
+    ArticleListItemComponent
   ],
   imports: [
     BrowserModule
