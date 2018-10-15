396bca768bfd2e77a8406441aaebe8dabcabcbfc
Akshat Gupta <akshat.gupta.241294@gmail.com>
2018-10-15 09:39:34 +0530

    added tagbox component

---
+++ src/app/tagbox/tagbox.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tagbox',
+  templateUrl: './tagbox.component.html',
+  styleUrls: ['./tagbox.component.css']
+})
+export class TagboxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
