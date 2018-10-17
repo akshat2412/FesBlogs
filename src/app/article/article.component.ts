960f532c627c3f18077fcd782d90d59bc1f5db8d
Akshat Gupta <akshat.gupta.241294@gmail.com>
2018-10-17 09:29:09 +0530

    added error handling code

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../api.service';
 import { IArticle } from 'src/Models/IArticle';
-import {ActivatedRoute} from '@angular/router';
+import {Router, ActivatedRoute} from '@angular/router';
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
@@ -9,11 +9,13 @@ import {ActivatedRoute} from '@angular/router';
 })
 export class ArticleComponent implements OnInit {
   articleData: IArticle;
-  constructor(private apiService: ApiService, private route: ActivatedRoute) { }
+  constructor(private apiService: ApiService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
     this.apiService.getArticle(this.route.snapshot.params['slug'])
-      .subscribe((data) => this.articleData = data['article']);
+      .subscribe(
+        (data) => this.articleData = data['article'],
+        (error) => this.router.navigate(['/404'])
+      );
   }
-
 }
