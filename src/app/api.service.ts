72ae33c37ea43069fd67d23f33fbdd94194610c7
Akshat Gupta <akshat.gupta.241294@gmail.com>
2018-10-18 11:34:01 +0530

    cleaned code and added more functions for api calls

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,23 +1,32 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-
+import { HttpClient } from '@angular/common/http';
+import { HttpParams } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-  url = 'https://conduit.productionready.io/api/';
+  url = 'https://conduit.productionready.io/api';
   constructor(private http: HttpClient) { }
 
   getArticles() {
-    return this.http.get(this.url + 'articles');
+    return this.http.get(this.url + '/articles');
   }
 
   getTags() {
-    return this.http.get(this.url + 'tags');
+    return this.http.get(this.url + '/tags');
   }
 
   getArticle(slug: string) {
     return this.http.get(`${this.url}/articles/${slug}`);
+  }
+
+  getComments(slug: string) {
+    return this.http.get(`${this.url}/articles/${slug}/comments`);
+  }
+
+  getArticlesByTag(tag: string) {
+    const params = new HttpParams().set('tag', tag);
+    return this.http.get(this.url + '/articles', {params: params});
   }
 }
