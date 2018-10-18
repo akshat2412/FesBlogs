69ee582211a626235de0e925ee5bfd1bf5fcf74c
Akshat Gupta <akshat.gupta.241294@gmail.com>
2018-10-18 11:42:48 +0530

    Added code for updating articles of selected tag.

--- src/app/article-list/article-list.component.ts
+++ src/app/article-list/article-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges,  } from '@angular/core';
 import { ApiService } from '../api.service';
 import { IArticle } from 'src/Models/Article.model';
 import { Router } from '@angular/router';
@@ -8,18 +8,44 @@ import { Router } from '@angular/router';
   templateUrl: './article-list.component.html',
   styleUrls: ['./article-list.component.css']
 })
-export class ArticleListComponent implements OnInit {
+export class ArticleListComponent implements OnInit, OnChanges {
 
   articles: IArticle[];
   pageNumber: Number = 1;
+  @Input() SelectedTag: string = null;
   constructor(private apiService: ApiService, private router: Router) { }
 
   ngOnInit() {
-    this.apiService.getArticles()
-      .subscribe(
-        (data) => this.articles = data['articles'],
-        (error) => this.router.navigate(['/404'])
-      );
+    if ( !!this.SelectedTag ) {
+      this.apiService.getArticlesByTag(this.SelectedTag)
+        .subscribe(
+          (data) => this.articles = data['articles'],
+          (error) => this.router.navigate(['/404'])
+        );
+    } else {
+      this.apiService.getArticles()
+        .subscribe(
+          (data) => this.articles = data['articles'],
+          (error) => this.router.navigate(['/404'])
+        );
+    }
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    this.articles = null;
+    if ( !!this.SelectedTag ) {
+      this.apiService.getArticlesByTag(this.SelectedTag)
+        .subscribe(
+          (data) => this.articles = data['articles'],
+          (error) => this.router.navigate(['/404'])
+        );
+    } else {
+      this.apiService.getArticles()
+        .subscribe(
+          (data) => this.articles = data['articles'],
+          (error) => this.router.navigate(['/404'])
+        );
+    }
   }
 
 }
