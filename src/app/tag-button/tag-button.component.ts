2cc28a93474a33e04bb873acc1eabfb14e58ed6e
Akshat Gupta <akshat.gupta.241294@gmail.com>
2018-10-18 11:37:59 +0530

    Added eventemitter in modular tagbox component

--- src/app/tag-button/tag-button.component.ts
+++ src/app/tag-button/tag-button.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, Input } from '@angular/core';
-
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-tag-button',
   templateUrl: './tag-button.component.html',
@@ -7,9 +7,14 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class TagButtonComponent implements OnInit {
   @Input() Text: string;
-  constructor() { }
+  @Output() ClickedButton = new EventEmitter();
+
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
+  clicked(tag) {
+    this.ClickedButton.emit(tag);
+  }
 }
--- src/app/tagbox/tagbox.component.ts
+++ src/app/tagbox/tagbox.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import {ApiService} from '../api.service';
 @Component({
   selector: 'app-tagbox',
@@ -6,12 +6,19 @@ import {ApiService} from '../api.service';
   styleUrls: ['./tagbox.component.css']
 })
 export class TagboxComponent implements OnInit {
+  @Input() SelectedTag: string;
+  @Output() ClickedButton = new EventEmitter();
   tags: string[];
+
   constructor(private apiService: ApiService) { }
 
   ngOnInit() {
     this.apiService.getTags()
       .subscribe((data) => this.tags = data['tags']);
+  }
+
+  EmitClickedButton(tag: string) {
+    this.ClickedButton.emit(tag);
   }
 
 }
