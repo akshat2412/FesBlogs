ec6dc08b0c10709471dafb3446c2477703c83cb2
Akshat Gupta <akshat.gupta.241294@gmail.com>
2018-10-18 11:43:25 +0530

    Added code for handling tag click

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.css']
 })
 export class LandingComponent implements OnInit {
-
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
+  HandleTagClick(ClickedTag: string) {
+    this.router.navigate(['/articles', ClickedTag]);
+  }
 }
