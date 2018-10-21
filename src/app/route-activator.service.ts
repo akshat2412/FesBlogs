29e55e50e42bec74ab7ff734fbaff9eddee8d1ea
Akshat Gupta <akshat.gupta.241294@gmail.com>
2018-10-21 19:12:37 +0530

    Recitified route gaurd

--- src/app/route-activator.service.ts
+++ src/app/route-activator.service.ts
@@ -1,15 +1,15 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, Router } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, Router, RouterStateSnapshot } from '@angular/router';
 
 import { ApiService } from './api.service';
 @Injectable({
   providedIn: 'root'
 })
-export class RouteActivatorService{
+export class RouteActivatorService implements CanActivate{
 
   constructor(private apiService: ApiService, private router: Router) { }
 
-  canActivate(route: ActivatedRouteSnapshot) {
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
     const isLoggedIn = this.apiService.isLoggedIn();
     if (!isLoggedIn) {
       this.router.navigate(['/login']);
