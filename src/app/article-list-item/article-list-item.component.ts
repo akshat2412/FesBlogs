8a244be7e1803b97437d1937030794cd6faf47cb
Akshat Gupta <akshat.gupta.241294@gmail.com>
2018-10-22 10:19:46 +0530

    Added favoriting articles functionality

--- src/app/article-list-item/article-list-item.component.ts
+++ src/app/article-list-item/article-list-item.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Router } from '@angular/router';
+
 import { IArticle } from 'src/Models/Article.model';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-article-list-item',
@@ -8,9 +11,27 @@ import { IArticle } from 'src/Models/Article.model';
 })
 export class ArticleListItemComponent implements OnInit {
   @Input() articleData: IArticle;
-  constructor() { }
+  isLoggedIn: boolean;
+
+  constructor(private apiService: ApiService, private router: Router) { }
 
   ngOnInit() {
+    this.isLoggedIn = this.apiService.isLoggedIn();
   }
 
+  toggleFavoriteArticle() {
+    if (this.articleData.favorited) {
+      this.apiService.unFavoriteArticle(this.articleData.slug)
+        .subscribe(
+          (data) => this.articleData = data['article'],
+          (error) => console.log(error)
+        );
+    } else {
+      this.apiService.favoriteArticle(this.articleData.slug)
+        .subscribe(
+          (data) => this.articleData = data['article'],
+          (error) => console.log(error)
+        );
+    }
+  }
 }
